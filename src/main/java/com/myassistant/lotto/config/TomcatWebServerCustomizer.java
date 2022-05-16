e3c0fd1032de418068a6b3706a53fbcdfabdc286
keisummer <hwangkyungha@naver.com>
2022-05-16 23:56:01 +0900

    chore: Try to solve 'Invalid character found in the request target' problem

--- src/main/java/com/myassistant/lotto/config/TomcatWebServerCustomizer.java
+++ src/main/java/com/myassistant/lotto/config/TomcatWebServerCustomizer.java
@@ -1,17 +1,20 @@
 package com.myassistant.lotto.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class TomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+public class TomcatWebServerCustomizer {
 
-    @Override
-    public void customize(TomcatServletWebServerFactory factory) {
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         factory.addConnectorCustomizers(connector -> {
-            connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}");
-            connector.setProperty("relaxedPathChars", "<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "|{}[]");
+            connector.setProperty("relaxedPathChars", "|{}[]");
         });
+        return factory;
     }
 }
