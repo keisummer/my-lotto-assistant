7e9e14825294c25e7dfd690f364f3fc0350a842e
keisummer <hwangkyungha@naver.com>
2022-05-17 00:02:16 +0900

    chore: Try to solve 'Invalid character found in the request target' problem

--- src/main/java/com/myassistant/lotto/adapter/in/web/LottoController.java
+++ src/main/java/com/myassistant/lotto/adapter/in/web/LottoController.java
@@ -1,12 +1,13 @@
 package com.myassistant.lotto.adapter.in.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LottoController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String hello() {
         return "Hello Lotto";
     }
